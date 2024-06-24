0c88fc30f62a94af5f66b69aa0692c150cd8c131
Manas Verma <122620440+Manasverma1111@users.noreply.github.com>
2024-06-24 20:38:23 +0530

    Add files via upload

---
+++ romanToInteger.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class romanToInteger {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a Roman numeral: ");
+        String romanNumeral = scanner.nextLine().trim();
+
+        romanToInteger solution = new romanToInteger();
+        int result = solution.romanToInt(romanNumeral);
+
+        System.out.println("Integer value of Roman numeral " + romanNumeral + " is: " + result);
+    }
+
+    public int romanToInt(String s) {
+        Map<Character, Integer> m = new HashMap<>();
+        m.put('I', 1);
+        m.put('V', 5);
+        m.put('X', 10);
+        m.put('L', 50);
+        m.put('C', 100);
+        m.put('D', 500);
+        m.put('M', 1000);
+
+        int ans = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1 && m.get(s.charAt(i)) < m.get(s.charAt(i + 1))) {
+                ans -= m.get(s.charAt(i));
+            } else {
+                ans += m.get(s.charAt(i));
+            }
+        }
+        return ans;
+    }
+}
