fca7968a0aff0b70f17b748f4ce8a0a694d8d25b
Manas Verma <122620440+Manasverma1111@users.noreply.github.com>
2024-06-24 21:21:41 +0530

    Add files via upload

---
+++ removeDuplicateFromArray.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements in the array:");
+        int n = scanner.nextInt();
+
+        int[] nums = new int[n];
+        System.out.println("Enter the elements of the array (sorted):");
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+
+        Solution solution = new Solution();
+        int newLength = solution.removeDuplicates(nums);
+
+        System.out.println("Array after removing duplicates:");
+        for (int i = 0; i < newLength; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+
+        scanner.close();
+    }
+}
