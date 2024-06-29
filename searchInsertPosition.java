e96bf35c15111279bcd3f35e778501e0a61a2d96
Manas Verma <122620440+Manasverma1111@users.noreply.github.com>
2024-06-29 09:37:35 +0530

    Add files via upload

---
+++ searchInsertPosition.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+class searchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements in the array:");
+        int n = scanner.nextInt();
+
+        int[] nums = new int[n];
+        System.out.println("Enter the elements of the array (sorted in ascending order):");
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+
+        System.out.println("Enter the target value:");
+        int target = scanner.nextInt();
+
+        searchInsertPosition solution = new searchInsertPosition();
+        int result = solution.searchInsert(nums, target);
+
+        System.out.println("The index of the target or the position to insert is: " + result);
+    }
+}
