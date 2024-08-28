564e3a5721d4f340cfa0e168fa5430bcfb74ca06
Manas Verma <122620440+Manasverma1111@users.noreply.github.com>
2024-08-28 22:48:16 +0530

    Delete sql_leco/cstmWhovisitedTrans.sql

--- sql_leco/cstmWhovisitedTrans.sql
+++
@@ -1,5 +0,0 @@
-SELECT customer_id, COUNT(customer_id) AS count_no_trans
-FROM visits
-LEFT JOIN Transactions ON Visits.visit_id = Transactions.visit_id
-WHERE transaction_id IS NULL
-GROUP BY customer_id;
