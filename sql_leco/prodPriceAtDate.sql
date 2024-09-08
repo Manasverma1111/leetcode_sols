570a39832f3777684a2600e9c036234520eb8e2f
Manas Verma <122620440+Manasverma1111@users.noreply.github.com>
2024-09-08 21:29:49 +0530

    Product Price at a Given Data
    Write a solution to find the prices of all products on 2019-08-16. Assume the price of all products before any change is 10.
    Return the result table in any order.

---
+++ sql_leco/prodPriceAtDate.sql
@@ -0,0 +1,16 @@
+SELECT DISTINCT product_id, 10 AS price 
+FROM Products 
+WHERE product_id NOT IN (
+    SELECT DISTINCT product_id 
+    FROM Products
+    WHERE change_date <= '2019-08-16' 
+)
+UNION
+SELECT product_id, new_price AS price 
+FROM Products
+WHERE (product_id, change_date) IN(
+    SELECT product_id, MAX(change_date) AS DATE 
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+    GROUP BY product_id
+);
