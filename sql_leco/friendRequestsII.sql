b061213f886d896567e430837ca2e48e9b280d5d
Manas Verma <122620440+Manasverma1111@users.noreply.github.com>
2024-09-10 23:19:34 +0530

    Friend Requests II: Who Has the Most Friends
    Write a solution to find the people who have the most friends and the most friends number.
    The test cases are generated so that only one person has the most friends.

---
+++ sql_leco/friendRequestsII.sql
@@ -0,0 +1,12 @@
+WITH base AS(
+    SELECT requester_id id 
+    FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id id FROM RequestAccepted)
+
+    SELECT id, COUNT(*) num
+    FROM base
+    GROUP BY 1
+    ORDER BY 2 DESC
+    LIMIT 1;
+    
