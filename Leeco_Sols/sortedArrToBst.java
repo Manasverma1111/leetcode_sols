349799ef7969d41a2fa039183772669e3f56b736
Manas Verma <122620440+Manasverma1111@users.noreply.github.com>
2024-09-14 22:36:38 +0530

    Update sortedArrToBst.java

--- Leeco_Sols/sortedArrToBst.java
+++ Leeco_Sols/sortedArrToBst.java
@@ -1,8 +1,17 @@
-package com.manas.leetco;
-
-import com.manas.leetco.*;
 import java.util.Scanner;
 
+  class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+ }
 
 public class sortedArrToBst {
 
