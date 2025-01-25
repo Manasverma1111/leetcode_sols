e1b00780652e362f0712a00087f650824396c32e
Manas Verma <122620440+Manasverma1111@users.noreply.github.com>
2025-01-25 23:20:35 +0530

    Unique Binary Search Trees II

---
+++ LeetCode/Unique-Binary-Search-Trees-II/Solution.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        
+        if (n == 0) {
+            return new ArrayList<>();
+        }
+        return generateTrees(1, n);
+    }
+
+    private List<TreeNode> generateTrees(int start, int end) {
+
+        List<TreeNode> res = new ArrayList<>();
+
+        if (start > end) {
+            res.add(null);
+            return res;
+        }
+
+        for (int i = start; i <= end; i++) {
+            
+            List<TreeNode> leftSubTree = generateTrees(start, i - 1);
+            List<TreeNode> rightSubTree = generateTrees(i + 1, end);
+
+            for (TreeNode left : leftSubTree) {
+                for (TreeNode right : rightSubTree) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+
+                    res.add(root);
+                }
+            }
+        }
+
+        return res;
+    }
+}
