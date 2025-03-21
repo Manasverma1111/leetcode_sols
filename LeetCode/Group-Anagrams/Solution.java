006ca3b52b8e8f8e161ef4e8f229f24290011d32
Manas Verma <122620440+Manasverma1111@users.noreply.github.com>
2025-03-21 22:59:27 +0530

    Group Anagrams

---
+++ LeetCode/Group-Anagrams/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+        if (strs == null || strs.length == 0) return new ArrayList<>();
+
+        Map<String, List<String>> anagramMap = new HashMap<>();
+
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+
+            anagramMap.putIfAbsent(sortedStr, new ArrayList<>());
+            anagramMap.get(sortedStr).add(str);
+        }
+
+        return new ArrayList<>(anagramMap.values());
+    }
+}
