666a2b16634296743265e74b7e337fe1263b1e66
Manas Verma <122620440+Manasverma1111@users.noreply.github.com>
2025-03-31 23:33:39 +0530

    Remove Outermost Parantheses

---
+++ LeetCode/Remove-Outermost-Parentheses/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuilder res = new StringBuilder();
+        int openCount = 0;
+
+        for (char ch : s.toCharArray()) {
+            if (ch =='(') {
+                if (openCount > 0) {
+                    res.append(ch);
+                } openCount++;
+            } else { 
+                openCount--;
+                if (openCount > 0) {
+                    res.append(ch);
+                }
+            }
+        }
+
+        return res.toString();
+    }
+}
