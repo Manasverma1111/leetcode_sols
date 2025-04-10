304e88ba0102559fe279a385042a79855e19f0fd
Manas Verma <122620440+Manasverma1111@users.noreply.github.com>
2025-04-10 23:24:59 +0530

    Subtree of Another Tree

--- LeetCode/Subtree-of-Another-Tree/Solution.java
+++ LeetCode/Subtree-of-Another-Tree/Solution.java
@@ -1,3 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        
+        if (root == null) {
+            return false;
         }
 
         if (isIdentical(root, subRoot)) {
@@ -8,3 +28,14 @@
     }
 
     public boolean isIdentical(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        }
+
+        if (s == null || t == null || s.val != t.val){
+            return false;
+        }
+
+        return isIdentical(s.left, t.left) && isIdentical(s.right, t.right);
+    }
+}
