6538e501c87304fd71d2285a2a696630070c0132
Manas Verma <122620440+Manasverma1111@users.noreply.github.com>
2025-04-11 23:20:08 +0530

    Count Symmetric Integers

--- LeetCode/-Count-Symmetric-Integers/Solution.java
+++ LeetCode/-Count-Symmetric-Integers/Solution.java
@@ -1,3 +1,13 @@
+class Solution {
+    public int countSymmetricIntegers(int low, int high) {
+        int count = 0;
+        
+        for (int num = low; num <= high; num++) {
+            int len = getLength(num);
+            if (len % 2 != 0) continue; 
+
+            int n = len / 2;
+            int sum1 = 0, sum2 = 0;
             int temp = num;
 
             for (int i = 0; i < n; i++) {
@@ -8,3 +18,22 @@
             for (int i = 0; i < n; i++) {
                 sum1 += temp % 10;
                 temp /= 10;
+            }
+
+            if (sum1 == sum2) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private int getLength(int num) {
+        int len = 0;
+        while (num > 0) {
+            len++;
+            num /= 10;
+        }
+        return len;
+    }
+}
