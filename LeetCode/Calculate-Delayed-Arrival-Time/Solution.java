0f813c9489d70271af6729f703fa276f3e96591d
Manas Verma <122620440+Manasverma1111@users.noreply.github.com>
2025-04-14 22:20:28 +0530

    Calculate Delayed Arrival Time

--- LeetCode/Calculate-Delayed-Arrival-Time/Solution.java
+++ LeetCode/Calculate-Delayed-Arrival-Time/Solution.java
@@ -2,9 +2,6 @@ class Solution {
     public int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
 
         int totalTime = arrivalTime + delayedTime;
-        if (totalTime == 24){
-            return 0;
-        } else {
-            return totalTime % 24; 
-        }
+        return totalTime % 24;
     }
+}
