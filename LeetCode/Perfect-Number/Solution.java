752ab46b1b061a8aa839058322b6ad3aaeae2e14
Manas Verma <122620440+Manasverma1111@users.noreply.github.com>
2025-04-20 22:09:58 +0530

    Perfect Number

--- LeetCode/Perfect-Number/Solution.java
+++ LeetCode/Perfect-Number/Solution.java
@@ -7,7 +7,7 @@ class Solution {
         
         int sum = 1;
         int sqrt = (int)Math.sqrt(num);
-
+        
         for (int i = 2; i <= sqrt; i++){
             if (num % i == 0){
                 sum += i;
