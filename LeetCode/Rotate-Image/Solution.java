4fc8843d13d6689469593a9ad7d525ddac76d42c
Manas Verma <122620440+Manasverma1111@users.noreply.github.com>
2025-05-22 11:14:26 +0530

    Add solution for Rotate Image

--- LeetCode/Rotate-Image/Solution.java
+++ LeetCode/Rotate-Image/Solution.java
@@ -3,17 +3,14 @@ class Solution {
         
         int n = matrix.length;
 
-        // Step 1: Transpose the matrix
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                // Swap elements at (i, j) and (j, i)
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
         }
 
-        // Step 2: Reverse each row
         for (int i = 0; i < n; i++) {
             reverseRow(matrix[i]);
         }
