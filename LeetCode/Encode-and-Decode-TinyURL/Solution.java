e77898d08dee5650991fed2bacce57d4281cf385
Manas Verma <122620440+Manasverma1111@users.noreply.github.com>
2025-05-28 22:09:48 +0530

    Add solution for Encode and Decode TinyURL

--- LeetCode/Encode-and-Decode-TinyURL/Solution.java
+++ LeetCode/Encode-and-Decode-TinyURL/Solution.java
@@ -1,36 +1,13 @@
 public class Codec {
-    private static final String BASE_HOST = "http://tinyurl.com/";
-    private static final String CHAR_SET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    private static final int BASE = CHAR_SET.length();
-    private int counter = 1;
-    private Map<String, String> shortToLong = new HashMap<>();
-    private Map<String, String> longToShort = new HashMap<>();
 
     // Encodes a URL to a shortened URL.
     public String encode(String longUrl) {
-        if (longToShort.containsKey(longUrl)) {
-            return BASE_HOST + longToShort.get(longUrl);
-        }
-        String shortKey = generateShortKey(counter++);
-        shortToLong.put(shortKey, longUrl);
-        longToShort.put(longUrl, shortKey);
-        return BASE_HOST + shortKey;
+        return longUrl;
     }
 
     // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
-        String shortKey = shortUrl.replace(BASE_HOST, "");
-        return shortToLong.get(shortKey);
-    }
-
-    // Helper to convert an integer counter to a short key using base62 encoding
-    private String generateShortKey(int num) {
-        StringBuilder sb = new StringBuilder();
-        while (num > 0) {
-            sb.append(CHAR_SET.charAt(num % BASE));
-            num /= BASE;
-        }
-        return sb.reverse().toString();
+        return shortUrl;
     }
 }
 
