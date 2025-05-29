b35c0372d07a41385fde30cde3d6fd660890b3d2
Manas Verma <122620440+Manasverma1111@users.noreply.github.com>
2025-05-29 22:00:53 +0530

    Add solution for Generate a String With Characters That Have Odd Counts

--- LeetCode/Generate-a-String-With-Characters-That-Have-Odd-Counts/Solution.java
+++ LeetCode/Generate-a-String-With-Characters-That-Have-Odd-Counts/Solution.java
@@ -1,19 +1,21 @@
 class Solution {
     public String generateTheString(int n) {
+
+        return n % 2 == 0 ? "a".repeat(n - 1) + "b" : "a".repeat(n); 
         
-        StringBuilder resStr = new StringBuilder();
+        // StringBuilder resStr = new StringBuilder();
 
-        if (n % 2 == 1) {
-            for (int i = 0; i < n; i++) {
-                resStr.append('a');
-            } 
-        } else {
-            for (int i = 0; i < n - 1; i++) {
-                resStr.append('a');
-            }
-            resStr.append('b');
-        }
+        // if (n % 2 == 1) {
+        //     for (int i = 0; i < n; i++) {
+        //         resStr.append('a');
+        //     } 
+        // } else {
+        //     for (int i = 0; i < n - 1; i++) {
+        //         resStr.append('a');
+        //     }
+        //     resStr.append('b');
+        // }
 
-        return resStr.toString();
+        // return resStr.toString();
     }
 }
