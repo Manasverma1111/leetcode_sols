5ce2f75237ef9a46619dd59c32871a07c261de6d
Manas Verma <122620440+Manasverma1111@users.noreply.github.com>
2025-05-31 21:53:30 +0530

    Add solution for Count Items Matching a Rule

---
+++ LeetCode/Count-Items-Matching-a-Rule/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        
+        int keyIndex = 0;
+
+        if (ruleKey.equals("color")) {
+            keyIndex = 1;
+        } else if (ruleKey.equals("name")) {
+            keyIndex = 2;
+        }
+
+        int count = 0;
+
+        for (List<String> item : items) {
+            if (item.get(keyIndex).equals(ruleValue)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
