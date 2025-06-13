b7de5466c6d1225e7303281ac0bfe04ce4be7735
Manas Verma <122620440+Manasverma1111@users.noreply.github.com>
2025-06-13 09:39:52 +0530

    Add solution for Fibonacci Number

--- LeetCode/Fibonacci-Number/Solution.java
+++ LeetCode/Fibonacci-Number/Solution.java
@@ -1,18 +1,10 @@
 class Solution {
     public int fib(int n) {
         
-       if (n == 0) return 0;
-        if (n == 1) return 1;
-
-        int a = 0;
-        int b = 1; 
-
-        for (int i = 2; i <= n; i++) {
-            int temp = a + b; 
-            a = b;            
-            b = temp;
+        if (n <= 1) {
+            return n;
         }
 
-        return b; 
+        return fib(n-1) + fib(n-2);
     }
 }
