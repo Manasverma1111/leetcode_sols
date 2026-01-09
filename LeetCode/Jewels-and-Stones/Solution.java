eb690955c8f6ae30f9ab4be688e5b36c29b5d557
Manas Verma <122620440+Manasverma1111@users.noreply.github.com>
2026-01-09 20:44:37 +0530

    Add solution for Jewels and Stones

--- LeetCode/Jewels-and-Stones/Solution.java
+++ LeetCode/Jewels-and-Stones/Solution.java
@@ -1,20 +1,20 @@
-class Solution {
-    public int numJewelsInStones(String jewels, String stones) {
-        
-        HashSet<Character> jewelSet = new HashSet<>();
-
-        for (char jewel : jewels.toCharArray()) {
-            jewelSet.add(jewel);
-        }
-
-        int count = 0;
-
-        for (char stone : stones.toCharArray()) {
-            if (jewelSet.contains(stone)) {
-                count++;
-            }
-        }
-
-        return count;
-    }
-}
+1class Solution {
+2    public int numJewelsInStones(String jewels, String stones) {
+3        
+4        HashSet<Character> jewelSet = new HashSet<>();
+5
+6        for (char jewel : jewels.toCharArray()) {
+7            jewelSet.add(jewel);
+8        }
+9
+10        int count = 0;
+11
+12        for (char stone : stones.toCharArray()) {
+13            if (jewelSet.contains(stone)) {
+14                count++;
+15            }
+16        }
+17
+18        return count;
+19    }
+20}
