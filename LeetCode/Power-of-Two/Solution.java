b5bfb0b032260d7d07e6e190350a335eb340fa15
Manas Verma <122620440+Manasverma1111@users.noreply.github.com>
2026-01-10 22:16:09 +0530

    Add solution for Power of Two

--- LeetCode/Power-of-Two/Solution.java
+++ LeetCode/Power-of-Two/Solution.java
@@ -1,6 +1,10 @@
-class Solution {
-    public boolean isPowerOfTwo(int n) {
- 
-        return n > 0 && (n & (n - 1)) == 0;
-    }
-}
+1class Solution {
+2    public boolean isPowerOfTwo(int n) {
+3        
+4        if (n > 0 && ((n & n - 1) == 0)) {
+5            return true;
+6        }
+7
+8        return false;
+9    }
+10}
