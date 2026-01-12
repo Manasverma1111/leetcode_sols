8ea1a33a0bdfeac03b56ecefa1e1c27fa2c76410
Manas Verma <122620440+Manasverma1111@users.noreply.github.com>
2026-01-12 20:26:34 +0530

    Add solution for Missing Number

--- LeetCode/Missing-Number/Solution.java
+++ LeetCode/Missing-Number/Solution.java
@@ -1,18 +1,14 @@
-class Solution {
-    public int missingNumber(int[] nums) {
-        
-        int n = nums.length;
-        int xor = 0;
-
-        for(int i = 0; i <= n; i++){
-            xor ^= i;
-        }
-
-        for (int num : nums){
-            xor ^= num;
-        }
-
-        return xor;
-
-    }
-}
+1class Solution {
+2    public int missingNumber(int[] nums) {
+3        
+4        int n = nums.length;
+5        int XOR = n;
+6
+7        for (int i = 0; i < n; i++) {
+8            XOR ^= i;
+9            XOR ^= nums[i];
+10        }
+11
+12        return XOR;
+13    }
+14}
