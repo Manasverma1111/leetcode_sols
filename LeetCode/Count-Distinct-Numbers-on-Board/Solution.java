e803a9a4649105787e4ec6e26249b70e1b3c8cfe
Manas Verma <122620440+Manasverma1111@users.noreply.github.com>
2026-01-14 23:08:50 +0530

    Add solution for Count Distinct Numbers on Board

--- LeetCode/Count-Distinct-Numbers-on-Board/Solution.java
+++ LeetCode/Count-Distinct-Numbers-on-Board/Solution.java
@@ -1,5 +1,6 @@
-class Solution {
-    public int distinctIntegers(int n) {
-        return Math.max(n - 1, 1);
-    }
-}
+1class Solution {
+2    public int distinctIntegers(int n) {
+3        return Math.max(n - 1, 1);
+4    }
+5}
+6
