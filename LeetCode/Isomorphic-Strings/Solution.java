1da87476f0637c0ee756399ed056d10528e43328
Manas Verma <122620440+Manasverma1111@users.noreply.github.com>
2026-01-19 21:57:44 +0530

    Add solution for Isomorphic Strings

--- LeetCode/Isomorphic-Strings/Solution.java
+++ LeetCode/Isomorphic-Strings/Solution.java
@@ -1,30 +1,28 @@
 1class Solution {
 2    public boolean isIsomorphic(String s, String t) {
 3        
-4         if (s.length() != t.length()) {
+4        if (s.length() != t.length()) {
 5            return false;
 6        }
 7
-8        int[] mapST = new int[256];
-9        int[] mapTS = new int[256];
-10        
-11        for (int i = 0; i < 256; i++) {
-12            mapST[i] = -1;
-13            mapTS[i] = -1;
-14        }
-15
-16        for (int i = 0; i < s.length(); i++) {
-17            char c1 = s.charAt(i);
-18            char c2 = t.charAt(i);
-19
-20            if (mapST[c1] == -1 && mapTS[c2] == -1) {
-21                mapST[c1] = c2; 
-22                mapTS[c2] = c1; 
-23            } else if (mapST[c1] != c2 || mapTS[c2] != c1) {
-24                return false;
-25            }
-26        }
-27        
-28        return true;
-29    }
-30}
+8        int[] mapst = new int[256];
+9        int[] mapts = new int[256];
+10
+11        for (int i = 0; i < s.length(); i++) {
+12            char ch1 = s.charAt(i);
+13            char ch2 = t.charAt(i);
+14
+15            if (mapst[ch1] != 0 && mapst[ch1] != ch2) {
+16                return false;
+17            }
+18            if (mapts[ch2] != 0 && mapts[ch2] != ch1) {
+19                return false;
+20            }
+21
+22            mapst[ch1] = ch2;
+23            mapts[ch2] = ch1;
+24        }
+25
+26        return true;
+27    }
+28}
