e2b831a678cb6f0292ecd76c4a23278536019120
Manas Verma <122620440+Manasverma1111@users.noreply.github.com>
2026-01-21 22:26:14 +0530

    Add solution for Defanging an IP Address

--- LeetCode/Defanging-an-IP-Address/Solution.java
+++ LeetCode/Defanging-an-IP-Address/Solution.java
@@ -1,6 +1,7 @@
-class Solution {
-    public String defangIPaddr(String address) {
-        
-        return address.replace(".", "[.]");
-    }
-}
+1class Solution {
+2    public String defangIPaddr(String address) {
+3        
+4        String newStr = address.replace(".", "[.]");
+5        return newStr;
+6    }
+7}
