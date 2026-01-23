8397dc574c8d06a1636e08eeeb22796f8d888a5e
Manas Verma <122620440+Manasverma1111@users.noreply.github.com>
2026-01-23 22:21:33 +0530

    Add solution for Search in a Binary Search Tree

--- LeetCode/Search-in-a-Binary-Search-Tree/Solution.java
+++ LeetCode/Search-in-a-Binary-Search-Tree/Solution.java
@@ -16,34 +16,16 @@
 16class Solution {
 17    public TreeNode searchBST(TreeNode root, int val) {
 18        
-19        if (root == null || root.val == val) {
-20            return root;
-21        }
-22
-23        if (val < root.val) {
-24            return searchBST(root.left, val);
-25        }
-26
-27        return searchBST(root.right, val);
-28    }
-29
-30    public void printTree(TreeNode root) {
-31        if (root == null) {
-32            System.out.println("[]");
-33            return;
-34        }
-35
-36        Queue<TreeNode> que = new LinkedList<>();
-37        que.offer(root);
-38        while (!que.isEmpty()) {
-39            TreeNode node = que.poll();
-40            if (node == null) {
-41                System.out.print(node.val + " ");
-42                que.offer(node.left);
-43                que.offer(node.right);
-44            } else {
-45                System.out.print("null");
-46            }
-47        }
-48    }
-49}
+19        while (root != null) {
+20            if (root.val == val) {
+21                return root;
+22            } else if ( val < root.val) {
+23                root = root.left;
+24            } else {
+25                root = root.right;
+26            }
+27        }
+28
+29        return null;
+30    }
+31}
