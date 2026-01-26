13a9e13fdf5d9c19fa6c40c907825106455a5d4d
Manas Verma <122620440+Manasverma1111@users.noreply.github.com>
2026-01-26 23:39:19 +0530

    Add solution for Island Perimeter

--- LeetCode/Island-Perimeter/Solution.java
+++ LeetCode/Island-Perimeter/Solution.java
@@ -1,32 +1,32 @@
-class Solution {
-    public int islandPerimeter(int[][] grid) {
-        
-        int rows = grid.length;
-        int cols = grid[0].length;
-        int perim = 0;
-
-        for (int i = 0; i < rows; i++){
-            for (int j = 0; j < cols; j++){
-                
-                if (grid[i][j] == 1){
-                    perim += 4;
-
-                    if (i > 0 && grid[i - 1][j] == 1){
-                        perim--;
-                    }
-                    if (i < rows - 1 && grid[i + 1][j] == 1){
-                        perim--;
-                    }
-                    if (j > 0 && grid[i][j - 1] == 1){
-                        perim--;
-                    }
-                    if (j < cols - 1 && grid[i][j + 1] == 1){
-                        perim--;
-                    }
-                }
-            }
-        }
-
-        return perim;
-    }
-}
+1class Solution {
+2    public int islandPerimeter(int[][] grid) {
+3        
+4        int rows = grid.length;
+5        int cols = grid[0].length;
+6        int perim = 0;
+7
+8        for (int i = 0; i < rows; i++){
+9            for (int j = 0; j < cols; j++){
+10                
+11                if (grid[i][j] == 1){
+12                    perim += 4;
+13
+14                    if (i > 0 && grid[i - 1][j] == 1){
+15                        perim--;
+16                    }
+17                    if (i < rows - 1 && grid[i + 1][j] == 1){
+18                        perim--;
+19                    }
+20                    if (j > 0 && grid[i][j - 1] == 1){
+21                        perim--;
+22                    }
+23                    if (j < cols - 1 && grid[i][j + 1] == 1){
+24                        perim--;
+25                    }
+26                }
+27            }
+28        }
+29
+30        return perim;
+31    }
+32}
