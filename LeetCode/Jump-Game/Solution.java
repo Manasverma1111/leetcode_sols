723f1a378a150970a5d97b710897437884be5e14
Manas Verma <122620440+Manasverma1111@users.noreply.github.com>
2026-03-09 15:12:27 +0530

    Add solution for Jump Game

--- LeetCode/Jump-Game/Solution.java
+++ LeetCode/Jump-Game/Solution.java
@@ -1,15 +1,20 @@
 1class Solution {
 2    public boolean canJump(int[] nums) {
-3        int maxReach = 0; 
-4        int target = nums.length - 1; 
-5        for (int i = 0; i <= maxReach; i++) {
-6            maxReach = Math.max(maxReach, i + nums[i]);
-7
-8            if (maxReach >= target) {
-9                return true;
-10            }
-11        }
+3        
+4         int maxReach = 0; 
+5
+6        for (int i = 0; i < nums.length; i++) {
+7            if (i > maxReach) {
+8                return false;
+9            }
+10
+11            maxReach = Math.max(maxReach, i + nums[i]);
 12
-13        return false;
-14    }
-15}
+13            if (maxReach >= nums.length - 1) {
+14                return true;
+15            }
+16        }
+17
+18        return false; 
+19    }
+20}
