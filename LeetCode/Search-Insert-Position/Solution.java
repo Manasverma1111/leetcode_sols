9d6d02d0d0e46f4d59f806f65a9d22d7df322856
Manas Verma <122620440+Manasverma1111@users.noreply.github.com>
2026-07-12 12:16:39 +0530

    Add solution for Search Insert Position

--- LeetCode/Search-Insert-Position/Solution.java
+++ LeetCode/Search-Insert-Position/Solution.java
@@ -2,18 +2,17 @@
 2    public int searchInsert(int[] nums, int target) {
 3        
 4        int left = 0;
-5        int right = nums.length - 1;
+5        int right = nums.length - 1;;
 6
 7        while (left <= right) {
 8            int mid = left + (right - left) / 2;
-9
-10            if (nums[mid] < target) {
-11                left = mid + 1;
-12            } else {
-13                right = mid - 1;
-14            }
-15        }
-16
-17        return left;
-18    }
-19}
+9            if (nums[mid] < target) {
+10                left = mid + 1;
+11            } else {
+12                right = mid - 1;
+13            }
+14        }
+15
+16        return left;
+17    }
+18}
