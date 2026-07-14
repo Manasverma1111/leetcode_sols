99732bb4d9fe1eb0784c4d23b92d828edee861d9
Manas Verma <122620440+Manasverma1111@users.noreply.github.com>
2026-07-14 10:42:52 +0530

    Add solution for Richest Customer Wealth

--- LeetCode/Richest-Customer-Wealth/Solution.java
+++ LeetCode/Richest-Customer-Wealth/Solution.java
@@ -1,18 +1,1 @@
-class Solution {
-    public int maximumWealth(int[][] accounts) {
-        
-        int max = Integer.MIN_VALUE;
-        for (int[] num1 : accounts) {
-            int sum = 0;
-            for (int num2 : num1) {
-                sum += num2;
-            }
-
-            if (sum > max) {
-                max = sum;
-            }
-        }
-
-        return max;
-    }
-}
+1st customer has wealth = 1 + 2 + 3 = 6
